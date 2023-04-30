16418adc66472772e6c03de4fd5c45c2f9b3a0b8
Димитрија Тимески <32930367+DimitrijaT@users.noreply.github.com>
2023-04-30 16:56:49 +0200

    Cypress test - Add and Delete item from Basket

--- cypress/e2e/products.cy.ts
+++ cypress/e2e/products.cy.ts
@@ -1,26 +1,32 @@
 import { LoginMethods } from "cypress/pageObjects/login/login.methods";
 import { ProductMethods } from "cypress/pageObjects/products/products.methods";
 
-describe('template spec', () => {
+describe("template spec", () => {
   var login = new LoginMethods();
   var products = new ProductMethods();
 
   beforeEach(function () {
-    login.navigateToLoginAndCloseDialog('http://localhost:3000/login#/login');
-    login.login('sanja.gorgievska@gmail', '#Newuser12#');
+    login.navigateToLoginAndCloseDialog("http://localhost:3000/login#/login");
+    login.login("dimitrijatimeskidimitrija@juice-sh.op", "cypress12345");
     login.verifySuccessfullLogin();
-  })
+  });
 
-  it('User should be able to add item into basket', () => {
-    cy.get('.mat-search_icon-search').click();
-    cy.get('#mat-input-0').type('apple juice');
-    cy.get('#mat-input-0').type('{enter}');
+  it("User should be able to add item into basket", () => {
+    cy.get(".mat-search_icon-search").click();
+    cy.get("#mat-input-0").type("apple juice");
+    cy.get("#mat-input-0").type("{enter}");
     cy.get('[aria-label="Add to Basket"]').click();
-    cy.get('.fa-3x.warn-notification').should('have.text', '1');
-  })
+    cy.get(".fa-3x.warn-notification").should("have.text", "1");
+  });
 
-  it('User should be able to add item into basket with POM', () => {
-    products.addItemToBasket('apple juice');
-    products.verifyItemAddedToBasket('1');
-  })
-})
+  it("User should be able to remove item from basket", () => {
+    cy.get('[class="mat-focus-indicator buttons mat-button mat-button-base ng-star-inserted"]').click();
+    cy.get('[class="mat-focus-indicator mat-icon-button mat-button-base"]').click();
+    cy.get(".fa-3x.warn-notification").should("have.text", "0");
+  });
+
+  // it("User should be able to add item into basket with POM", () => {
+  //   products.addItemToBasket("apple juice");
+  //   products.verifyItemAddedToBasket("1");
+  // });
+});
